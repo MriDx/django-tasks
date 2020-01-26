bca49a056bd50a79c2cfe60aca504862436a2aba
krishNa <krishnamohan541@gmail.com>
2020-01-27 01:59:10 +0530

    django-task

---
+++ authenticates/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phone_field import PhoneField
+
+
+
+# Create your models here.
+
+# class MyUser(User):
+#     class Meta:
+#         proxy = True
+
+#     def __str__(self):
+#         return str(self.id)
+
+
+class Profile(models.Model):
+    user_id = models.ForeignKey(User, related_name='profiles', on_delete=models.CASCADE)
+    gender = models.CharField(max_length=12, blank=False)
+    mobile_no = PhoneField(max_length=20, blank=True)
+    #email = models.EmailField(max_length=150)
+    signup_confirmation = models.BooleanField(default=False)
+    about = models.CharField(max_length=150, blank = True)
+    photo = models.ImageField(upload_to='', blank=False)
+    #TODO chanage location field here #Have to think of it... Experimental
+    location = models.CharField("Office location", blank=False, null=False, max_length = 50)
+    # created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return self.user.username
+
+    class Meta:
+        unique_together  = ["user_id", "gender", "mobile_no", "location"]
+        ordering = ['user_id']
+
+
+class List_group(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(unique=True, null=False, max_length=150)
+    admin = models.ForeignKey(User, related_name='lists_groups', on_delete=models.CASCADE)
+    is_group = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=['name', 'admin', 'is_group'], name='group_and_list_unique_key')
+        ]
+
+    def __str__(self):
+        return self.name
